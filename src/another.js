27c97b1ebd17e8d31c0002381cc0ca2373bc4575
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2020-05-31 16:06:30 -0300

    Projeto estudo webpack 4
    - Babel version compativel
    - HtmlPlugin version compativel

---
+++ src/another.js
@@ -0,0 +1,1 @@
+export default () => console.log('Webpack Zero Config Works!');
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import anotherFunction from './another';
+
+const testTranspile = () => console.log('Arrow Function Test');
+
+anotherFunction();
+testTranspile();
